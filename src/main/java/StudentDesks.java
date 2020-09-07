5c361f148f4e5d153f59e3d696b73bfa16f4aa38
Jaden Arceneaux <jaden.236829@nv.ccsd.net>
2020-09-07 05:16:48 +0000

    Completed Student Desk

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 class StudentDesks {
     public static void main(String[] args) {
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
+        double class1 = Math.round(scanner.nextDouble() / 2);
+        double class2 = Math.round(scanner.nextDouble() / 2);
+        double class3 = Math.round(scanner.nextDouble() / 2);
         
+        System.out.print(class1 + class2 + class3);
 
-        /*
-         *  your code goes here
-         */
-
-
-        // closing the scanner object
         scanner.close();
     }
 }
